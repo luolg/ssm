dd82c7c25d7a3aadf200404f0479b5a446fdbc43
luolg <luolg8@gmail.com>
2022-06-11 00:42:16 +0800

    add last commit

--- 017-ssm/src/main/java/com/luolg/service/impl/AccountServiceImpl.java
+++ 017-ssm/src/main/java/com/luolg/service/impl/AccountServiceImpl.java
@@ -7,42 +7,60 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
-@Service
+//@Service
+//public class AccountServiceImpl implements AccountService {
+//    @Override
+//    public void save(Account account) throws IOException {
+//        try {
+//            InputStream resourceAsStream = Resources.getResourceAsStream("sqlMapConfig.xml");
+//            SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+//            SqlSession sqlSession = sqlSessionFactory.openSession();
+//            AccountMapper mapper = sqlSession.getMapper(AccountMapper.class);
+//            mapper.save(account);
+//            sqlSession.commit();
+//            sqlSession.close();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    @Override
+//    public List<Account> findAll() throws IOException {
+//        try {
+//            InputStream resourceAsStream = Resources.getResourceAsStream("sqlMapConfig.xml");
+//            SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+//            SqlSession sqlSession = sqlSessionFactory.openSession();
+//            AccountMapper mapper = sqlSession.getMapper(AccountMapper.class);
+//            List<Account> accountList = mapper.findAll();
+//            sqlSession.close();
+//            return accountList;
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        return null;
+//    }
+//}
+
+@Service("accountService")
 public class AccountServiceImpl implements AccountService {
+
+    @Autowired
+    private AccountMapper accountMapper;
+
     @Override
-    public void save(Account account) throws IOException {
-        try {
-            InputStream resourceAsStream = Resources.getResourceAsStream("sqlMapConfig.xml");
-            SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
-            SqlSession sqlSession = sqlSessionFactory.openSession();
-            AccountMapper mapper = sqlSession.getMapper(AccountMapper.class);
-            mapper.save(account);
-            sqlSession.commit();
-            sqlSession.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public void save(Account account) {
+        accountMapper.save(account);
     }
 
     @Override
-    public List<Account> findAll() throws IOException {
-        try {
-            InputStream resourceAsStream = Resources.getResourceAsStream("sqlMapConfig.xml");
-            SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
-            SqlSession sqlSession = sqlSessionFactory.openSession();
-            AccountMapper mapper = sqlSession.getMapper(AccountMapper.class);
-            List<Account> accountList = mapper.findAll();
-            sqlSession.close();
-            return accountList;
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
+    public List<Account> findAll()  {
+        return accountMapper.findAll();
     }
 }
---
+++ 017-ssm/src/main/resources/sql/run.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `account` (
+                           `id` int(11) NOT NULL AUTO_INCREMENT,
+                           `name` varchar(50) DEFAULT NULL,
+                           `money` double DEFAULT NULL,
+                           PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8
+
